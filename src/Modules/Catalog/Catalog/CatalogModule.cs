cb8b21e7ea4d03e692c2cda39ef5cfe7b8f4f8eb
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-16 14:37:12 +0300

    Added MigrateDatabaseAsync extension method to Shared project

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
 
 namespace Catalog;
 
@@ -18,17 +19,17 @@ public static class CatalogModule
 
 	public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
 	{
-
-		InitializeDatabaseAsync(app).GetAwaiter().GetResult();
+		//InitializeDatabaseAsync(app).GetAwaiter().GetResult();
+		app.UseMigration<CatalogDbContext>();
 		return app;
 	}
 
-	private static async Task InitializeDatabaseAsync(IApplicationBuilder app)
-	{
-		using var scope = app.ApplicationServices.CreateScope();
+	//private static async Task InitializeDatabaseAsync(IApplicationBuilder app)
+	//{
+	//	using var scope = app.ApplicationServices.CreateScope();
 
-		var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+	//	var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
 
-		await context.Database.MigrateAsync();
-	}
+	//	await context.Database.MigrateAsync();
+	//}
 }
