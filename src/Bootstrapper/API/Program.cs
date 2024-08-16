4f091edad84a481286bf6d8d594d44873d283b11
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-16 11:23:35 +0300

    Added extension methods to modules for applying registiration

--- src/Bootstrapper/API/Program.cs
+++ src/Bootstrapper/API/Program.cs
@@ -1,4 +1,18 @@
+using Basket;
+using Catalog;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services
+	.AddCatalogModule(builder.Configuration)
+	.AddBasketModule(builder.Configuration)
+	.AddOrderingModule(builder.Configuration);
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
 var app = builder.Build();
 
 app.Run();
