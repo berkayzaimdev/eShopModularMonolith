af53e6ae28fed5598996e0b678ed3909c63821f6
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-19 11:58:02 +0300

    Added CreateProductValidator

--- src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,24 @@
-﻿namespace Catalog.Products.Features.CreateProduct;
+﻿using FluentValidation;
+
+namespace Catalog.Products.Features.CreateProduct;
 
 public record CreateProductCommand(ProductDto Product)
 	: ICommand<CreateProductResult>;
+
 public record CreateProductResult(Guid Id);
+
+public class CreateProductValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductValidator()
+    {
+		RuleFor(x => x.Product.Name).NotEmpty().WithMessage("Name is required");
+		RuleFor(x => x.Product.Category).NotEmpty().WithMessage("Category are required");
+		RuleFor(x => x.Product.ImageFile).NotEmpty().WithMessage("Image is required");
+		RuleFor(x => x.Product.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
+
 internal class CreateProductHandler 
 	(CatalogDbContext dbContext)
 	: ICommandHandler<CreateProductCommand, CreateProductResult>
