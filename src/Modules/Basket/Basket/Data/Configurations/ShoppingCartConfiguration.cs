6ddf380fbfd8b5c6f3951dcc9ba81456ce84a525
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-19 14:42:52 +0300

    Added ShoppingCart and ShoppingCartItem configurations

---
+++ src/Modules/Basket/Basket/Data/Configurations/ShoppingCartConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.Data.Configurations;
+
+public class ShoppingCartConfiguration : IEntityTypeConfiguration<ShoppingCart>
+{
+	public void Configure(EntityTypeBuilder<ShoppingCart> builder)
+	{
+		builder.HasKey(e => e.Id);
+
+		builder.HasIndex(e => e.UserName)
+			.IsUnique();
+
+		builder.Property(e => e.UserName)
+			.IsRequired()
+			.HasMaxLength(100);
+	}
+}
