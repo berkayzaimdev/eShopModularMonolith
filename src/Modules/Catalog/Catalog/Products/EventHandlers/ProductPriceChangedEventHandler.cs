3f177fbccb630980b49dab42e679a790db6acfc5
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-19 11:17:49 +0300

    Created ProductCreated and ProductPriceChanged event handlers

---
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Products.EventHandlers;
+
+public class ProductPriceChangedEventHandler
+	(ILogger<ProductCreatedEventHandler> logger)
+	: INotificationHandler<ProductPriceChangedEvent>
+{
+	public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+	{ 
+		logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType());
+		return Task.CompletedTask;
+	}
+}
