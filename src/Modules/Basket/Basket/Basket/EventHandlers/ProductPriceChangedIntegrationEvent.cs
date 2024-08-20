4fc4003a0dd40f3a86327259d0be7b467c943e72
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-20 13:16:25 +0300

    Modified ProductPriceChangedIntegrationEventHandler for adding event handling logic

--- src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEvent.cs
+++ src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEvent.cs
@@ -1,13 +1,24 @@
-﻿namespace Basket.Basket.EventHandlers;
+﻿using Basket.Basket.Features.UpdateItemPriceInBasket;
+
+namespace Basket.Basket.EventHandlers;
 
 public class ProductPriceChangedIntegrationEventHandler
 	(ISender sender, ILogger<ProductPriceChangedIntegrationEventHandler> logger)
 	: IConsumer<ProductPriceChangedIntegrationEvent>
 {
-	public Task Consume(ConsumeContext<ProductPriceChangedIntegrationEvent> context)
+	public async Task Consume(ConsumeContext<ProductPriceChangedIntegrationEvent> context)
 	{
 		logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
 
-		return Task.CompletedTask;
+		var command = new UpdateItemPriceInBasketCommand(context.Message.ProductId, context.Message.Price);
+
+		var result = await sender.Send(command);
+
+		if (!result.IsSuccess)
+		{
+			logger.LogError("Error updating price in basket for product id: {ProductId}", context.Message.ProductId);
+		}
+
+		logger.LogInformation("Price for product id: {ProductId} updated in basket", context.Message.ProductId);
 	}
 }
