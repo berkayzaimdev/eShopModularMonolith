d1b3e71edd8217dbd9c530bde50e9b5821a1f331
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-21 11:17:21 +0300

    Added BasketCheckoutDto

---
+++ src/Modules/Basket/Basket/Basket/Features/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.Basket.Features.CheckoutBasket;
+public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckout)
+	: ICommand<CheckoutBasketResult>;
+public record CheckoutBasketResult();
+public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+{
+	public CheckoutBasketCommandValidator()
+	{
+	}
+}
+public class CheckoutBasketHandler
+{
+}
